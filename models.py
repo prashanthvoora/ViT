295afa64ec05b6e4409eabd6ec7be79693449709
Voora, Prashanth <prashanthx85@gmail.com>
2025-12-02 19:46:44 +0530

    Addition of bug fixes to base model
    Signed-off-by: Voora, Prashanth <prashanthx85@gmail.com>

--- models.py
+++ models.py
@@ -99,6 +99,10 @@ def deit_small_patch16_224(pretrained=False, **kwargs):
 
 @register_model
 def deit_base_patch16_224(pretrained=False, **kwargs):
+    kwargs.pop("pretrained_cfg", None)
+    kwargs.pop("pretrained_cfg_overlay", None)
+    kwargs.pop("cache_dir", None)
+
     model = VisionTransformer(
         patch_size=16, embed_dim=768, depth=12, num_heads=12, mlp_ratio=4, qkv_bias=True,
         norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
